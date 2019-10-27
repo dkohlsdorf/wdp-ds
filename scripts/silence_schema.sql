e32c5c0efa047e653506346efd13eff32dc452bb
Daniel Kohlsdorf <daniel.kohlsdorf@xing.com>
2019-10-27 15:08:05 +0100

    fixing memory problem with silence detector

--- scripts/silence_schema.sql
+++ scripts/silence_schema.sql
@@ -2,6 +2,7 @@ DROP TABLE IF EXISTS wdp_ds.not_silent;
 CREATE TABLE wdp_ds.not_silent (
        id           BIGINT PRIMARY KEY NOT NULL,
        encoding     BIGINT NOT NULL,
+       filename     VARCHAR(512),
        start        BIGINT,
        stop         BIGINT,       
        FOREIGN KEY (encoding)

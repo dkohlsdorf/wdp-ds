353fe8fb689aeab61044706d039449e5372fb976
DeepLearning VM <dkohlsdorf@gmail.com>
2019-11-02 15:25:16 +0000

    model v3.6

--- scripts/initial_model.sh
+++ scripts/initial_model.sh
@@ -10,7 +10,7 @@ python ml/model_analysis.py 32 data/demo/ predict_next models/lstm_v2/ data/cata
 
 # LSTM V3
 python ml/train_lstm_combined.py 128 128 data/catalogue/whistle_snippets/ data/catalogue/burst_snippet/ data/silence_011119/ data/2012/
-python ml/train_silence_detector.py 128 data/catalogue/whistle_snippets/ data/catalogue/burst_snippet/ data/catalogue/noise_snippets/ data/catalogue2011/noise data/catalogue2011/not_noise
+python ml/train_silence_detector.py 128 data/catalogue/whistle_snippets/ data/catalogue/burst_snippet/ data/catalogue/noise_snippets/ data/catalogue2011/noise/ data/catalogue2011/not_noise/
 python ml/model_analysis.py 128 data/demo/ predict_next models/lstm_v3/v3.6/ data/catalogue/whistle_snippets/ data/catalogue/burst_snippet/ data/catalogue/noise_snippets/
 
 

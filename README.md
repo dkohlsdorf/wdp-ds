4c1fc61de6ab13e4ba3f8c5405e697d82b50c413
DeepLearning VM <dkohlsdorf@gmail.com>
2019-11-11 08:44:08 +0000

    v4.3

--- README.md
+++ README.md
@@ -11,3 +11,8 @@ and communicate the results with domain experts.
 + 3) `models`: The models with a small report on their performance
 + 4) `scripts`: Collection of scripts that keep the project alive
 
+## Results So Far
+
+The best model I could come up with is an lstm/convnet based encoder
+decoder architecture trained as an auto encoder. Can be found in model
+`v4.1`
--- ml/train_lstm_auto_encoder.py
+++ ml/train_lstm_auto_encoder.py
@@ -8,9 +8,9 @@ import sys
 
 def encoder(in_shape, latent_dim):
         inp = Input(in_shape)
-        loc = Conv2D(256, kernel_size=(8, 8), activation='relu', padding='same')(inp) 
+        loc = Conv2D(128, kernel_size=(8, 8), activation='relu', padding='same')(inp) 
         loc = MaxPool2D(pool_size=(1, 256))(loc)
-        loc = Reshape((in_shape[0], 256))(loc)
+        loc = Reshape((in_shape[0], 128))(loc)
         x   = BatchNormalization()(loc)
         x   = Bidirectional(LSTM(128, return_sequences=True))(x)
         x   = LSTM(latent_dim)(x)            
@@ -22,11 +22,11 @@ def predictor(win, target_dim, output_dim):
     inp = Input((target_dim))
     x = Reshape((1, target_dim))(inp)
     x = ZeroPadding1D((0, win - 1))(x)
-    x = LSTM(256, return_sequences=True)(x)    
+    x = LSTM(128, return_sequences=True)(x)    
     x = Bidirectional(LSTM(128, return_sequences=True))(x)
     x = Reshape((win, 256, 1))(x)
-    x = Conv2DTranspose(256, kernel_size=(8, 8), activation='relu', padding='same')(x) 
-    x = Conv2DTranspose(1,   kernel_size=(1, 1), activation='relu', padding='same')(x) 
+    x = Conv2DTranspose(128, kernel_size=(8, 8), activation='relu', padding='same')(x) 
+    x = Conv2DTranspose(1,   kernel_size=(1, 1), activation='linear', padding='same')(x) 
     model = Model(inputs = [inp], outputs = [x])
     model.summary()
     return model
@@ -47,7 +47,7 @@ def ae_from_file(paths, win, latent):
     ae, enc  = auto_encoder((win, 256, 1), latent, 256 * win, win)
     w_before = enc.layers[1].get_weights()[0].flatten()
     x = np.stack([x for x in data_gen(paths, win)])
-    ae.fit(x = x, y = x, batch_size = 10, shuffle = True, epochs = 32)
+    ae.fit(x = x, y = x, batch_size = 100, shuffle = True, epochs = 128)
     w_after = enc.layers[1].get_weights()[0].flatten()
 
     print("DELTA W:", np.sum(np.square(w_before - w_after)))

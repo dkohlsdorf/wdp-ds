e26aa003fb33ebeae0376bce344d134821c107bc
Daniel Kohlsdorf <daniel.kohlsdorf@xing.com>
2019-11-13 10:51:24 +0100

    fixing pipeline adding embedder

--- ml_pipeline/pipeline.py
+++ ml_pipeline/pipeline.py
@@ -45,7 +45,7 @@ def train(folder, params, lable, model, batch_size=10, epochs=128):
     model: a keras model
     batch_size: size of the mini batch
     epochs: number of runs over the complete dataset
-    '''
+    '''            
     n_processed = 0
     for epoch in range(epochs):
         batch = []
@@ -54,9 +54,16 @@ def train(folder, params, lable, model, batch_size=10, epochs=128):
             total_loss = 0.0
             if len(batch) == batch_size:
                 x = np.stack([x.reshape(x.shape[0], x.shape[1], 1) for x, _ in batch])
-                y = np.stack([y.reshape(y.shape[0], y.shape[1], 1) for _, y in batch])
-                total_loss += model.train_on_batch(x=x, y=y)
-                batch = []      
+                if isinstance(batch[0][1], float):
+                    y = np.array([y for _, y in batch])
+                else:
+                    y = np.stack([y.reshape(y.shape[0], y.shape[1], 1) for _, y in batch])
+                loss = model.train_on_batch(x=x, y=y)
+                if isinstance(loss, float):
+                    total_loss += loss
+                else:
+                    total_loss += loss[0]
+                batch = []
                 if n_processed % 10 == 0:
                     print("#: {} EPOCH: {} LOSS: {}".format(n_processed, epoch, total_loss))
                     total_loss = 0.0
@@ -76,7 +83,6 @@ def train_silence(version_tag, input_folder, output_folder, params, encoder_file
     epochs: number of training epochs
     '''
     print("Training Auto Encoder: {}".format(version_tag))
-
     enc     = load_model(encoder_file)
     cls_sil = classifier(enc)
     train(input_folder, params, sil, cls_sil, batch, epochs)
@@ -93,10 +99,12 @@ def test_silence(version_tag, input_folder, output_folder, params, sil_file):
     params: window parameters
     sil_file: saved silence detector
     '''
-    sil = load_model(sil_file)
+    silence = load_model(sil_file)
     confusion = np.zeros((2,2))
-    for (x, y) in dataset(input_folder, params, sil, False):
-        _y = int(round(sil.predict(x.reshape(1, x.shape[0], x.shape[1], 1))[0]))
+    for (x, y,_,_,_) in dataset(input_folder, params, sil, False):
+        _y = int(np.round(silence.predict(x.reshape(1, x.shape[0], x.shape[1], 1))[0]))
+        y = int(y)
+        print(y, _y)
         confusion[y][_y] += 1
     np.savetxt('{}/confusion.csv'.format(output_folder), confusion)
     
@@ -141,7 +149,7 @@ def evaluate_encoder(version_tag, input_folder, output_folder, encoder_file, par
     enc = load_model(encoder_file)
     visualize_2dfilters(output_folder, enc, [1], n_rows = 8)    
     x = np.stack([x.reshape(x.shape[0], x.shape[1], 1) for (x,_,_,_,_) in dataset(
-        folder, params, no_label, False
+        input_folder, params, no_label, False
     )])
     visualize_embedding("{}/embeddings.png".format(output_folder), x, enc, k)
 
@@ -179,6 +187,6 @@ if __name__== "__main__":
         unsupervised = c['unsupervised']
         output       = c['output']        
         train_auto_encoder(version, unsupervised, output, params, latent, batch, epochs)
-        evaluate_encoder(version, silence, output, "encoder.h5", params, viz_k)
-        train_silence(version_tag, input_folder, output_folder, params, "encoder.h5", batch, epoch)
-        test_silence(version_tag, input_folder, output_folder, params, "sil.h5")        
+        evaluate_encoder(version, silence, output, "{}/encoder.h5".format(output), params, viz_k)
+        train_silence(version, silence, output, params, "{}/encoder.h5".format(output), batch, epochs)
+        test_silence(version, silence, output, params, "{}/sil.h5".format(output))        

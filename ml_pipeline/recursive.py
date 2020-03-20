954a196416557a47f6facfadcbb2ec321671c1f3
Daniel Kohlsdorf <daniel.kohlsdorf@xing.com>
2020-03-20 20:22:09 +0100

    recursive nn

--- ml_pipeline/recursive.py
+++ ml_pipeline/recursive.py
@@ -1,10 +1,20 @@
 import tensorflow as tf
 import numpy as np
 from random import shuffle
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, confusion_matrix
 
 
 def merge_encoder(n_in, n_classes):
+    '''
+    A semi supervised recursive neural network.
+    Given inputs a, b the network will construct 
+    a hidden layer and then reconstruct a and b
+    as well as predict the classes.
+
+    :params n_in: input dimension
+    :params n_classes: number of classes
+    :returns: the model
+    '''
     a       = tf.keras.layers.Input(n_in)
     b       = tf.keras.layers.Input(n_in)
     c       = tf.keras.layers.Concatenate()([a,b])
@@ -17,22 +27,28 @@ def merge_encoder(n_in, n_classes):
 
 
 class Node:
+    '''
+    A merge node with left and right children.
+    Holds other information such the embedding of the merge,
+    the ground truth label, the prediction of the network and
+    the score.
+    '''
     
-    def __init__(self, i, embedding, label, score, l = None, r = None):
-        self.i         = i
-        self.score     = score
-        self.embedding = embedding
-        self.left      = l
-        self.right     = r
-        self.label     = label
-        
-    def print(self, offset=""):
-        print("{} {} {} {}".format(offset, self.i, self.score, np.mean(self.embeding)))
-        if self.left is not None and self.right is not None:
-            self.left.print(offset + "\t")
-            self.right.print(offset + "\t")
+    def __init__(self, i, embedding, label, prediction, score, l = None, r = None):
+        self.i          = i
+        self.score      = score
+        self.embedding  = embedding
+        self.left       = l
+        self.right      = r
+        self.label      = label
+        self.prediction = prediction
 
-    def merge(self, other, merger, training = True):
+
+    def merge(self, other, merger):
+        '''
+        Merge this node and another using a merge neural network
+        score the merge as: score(reconstruction) + score(classififcation) + left_score + right_score
+        '''
         merged         = merger([self.embedding, other.embedding])
         h              = merged[0]
         c              = merged[1]
@@ -42,23 +58,36 @@ class Node:
             + tf.nn.softmax_cross_entropy_with_logits(y, c) \
             + self.score  \
             + other.score 
-        if training:
-          return Node(-1, h, self.label, score, self, other)
-        else:
-          return Node(-1, h, classification, score, self, other)
+        return Node(-1, h, self.label, classification, score, self, other)
 
 
 def ts2leafs(s, label, tokens, n_classes):
+    '''
+    Convert a labeled sequence to a sequence of leaf nodes
+
+    :param s: sequence
+    :param label: label for sequence
+    :param tokens: token for each symbol in the sequence
+    :param n_classes: number of classes
+    :returns: sequence of leaf nodes
+    '''
     sequence = []
     for i, x in enumerate(s):
         y = np.zeros((1, n_classes))
         y[0, label] = 1 
-        node = Node(i, tokens[x], y, tf.constant(0.0))
+        node = Node(i, tokens[x], y, None, tf.constant(0.0))
         sequence.append(node)
     return sequence
 
 
 def merge(x, m):
+    '''
+    merge a sequence to one node using the merge neural network
+
+    :param x: sequence
+    :param m: merge model
+    :returns: one node
+    '''
     while len(x) > 1:        
         min_loss = float('inf')
         min_node = None
@@ -103,10 +132,16 @@ class RecursiveNeuralNet:
                 with tf.GradientTape(watch_accessed_variables=True) as tape:
                     tape.watch(self.merger.variables) 
                     node = merge(x, self.merger)
-                    _y.append(node.label)
+                    _y.append(np.argmax(node.prediction))
                     y.append(labels[i])
                     acc = accuracy_score(y, _y)
-                    print("Score: {} {}/{} Acc:{} Epoch: {}".format(node.score[0], i, len(sequences), epoch))
+                    print("Score: {} {}/{} Epoch: {}".format(node.score[0], i, len(sequences), epoch))
+                    if len(y) == 10:
+                        print("Acc: {}".format(acc))
+                        print(confusion_matrix(y, _y))
+                        _y = []
+                        y  = []
+
                     g    = tape.gradient(node.score, self.merger.variables)
                     self.optimizer.apply_gradients(zip(g, self.merger.variables))
 

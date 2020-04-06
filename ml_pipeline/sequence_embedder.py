f6294b6885c7f8494cdf274a483cdc4a234b0626
dkohlsdorf <dkohlsdorf@gmail.com>
2020-04-06 11:11:10 +0200

    remove kmeans

--- ml_pipeline/sequence_embedder.py
+++ ml_pipeline/sequence_embedder.py
@@ -9,19 +9,17 @@ class SequenceEmbedder:
     embedding model
     """
 
-    def __init__(self, encoder, silence_detector, type_classifier, km, param):
+    def __init__(self, encoder, silence_detector, type_classifier, param):
         """
         :param encoder: a keras model In (?, T, D, 1) out (?, Latent)
         :param silence_detector: a keras model In(?, T, D, 1) out (?, 1)
         :param type_classifier: classifying spectrogram data into types
-        :param km: trained kmeans  
         :param param: windowing parameters
         """
         self.encoder = encoder
         self.silence_detector = silence_detector
         self.param = param
         self.type_classifier = type_classifier
-        self.km  = km
 
     def embed(self, filename):
         """
@@ -39,15 +37,13 @@ class SequenceEmbedder:
                 is_silence = self.silence_detector.predict(b)
                 types      = self.type_classifier.predict(b)                
                 embedding  = self.encoder.predict(b)
-                clusters   = self.km.predict(embedding)
                 for i in range(0, len(batch)):
                     if int(round(is_silence[i][0])) == 0:
-                        c = clusters[i]
                         t = np.argmax(types[i])                    
                         filename  = batch[i][1]
                         start     = batch[i][2]
                         stop      = batch[i][3]
-                        print('- Found non silent region {} {}:{} extracting embedding of size {} / {} with [{}, {}]'.format(filename, start / 48000, stop / 48000, embedding[i, :].shape, len(regions), t, c))
-                        regions.append((embedding[i, :], filename, start, stop, t, c))
+                        print('- Found non silent region {} {}:{} extracting embedding of size {} / {} with [{}]'.format(filename, start / 48000, stop / 48000, embedding[i, :].shape, len(regions), t))
+                        regions.append((embedding[i, :], filename, start, stop, t))
                 batch = []
         return regions

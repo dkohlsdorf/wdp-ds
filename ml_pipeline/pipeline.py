f8341550d63e96daedcab8200909be1fc120ef81
DeepLearning VM <dkohlsdorf@gmail.com>
2020-04-07 13:19:01 +0000

    cleaning analysis code delete tokens

--- ml_pipeline/pipeline.py
+++ ml_pipeline/pipeline.py
@@ -17,7 +17,7 @@ from plots import *
 from sequence_embedder import *
 from audio_collection import *
 from audio import * 
-#from structured import *
+from structured import *
 from utils import * 
 from data_augmentation import * 
 

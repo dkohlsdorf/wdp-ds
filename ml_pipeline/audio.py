3510c826dc5868f9730ec2a52afc7a588efda71a
Daniel Kohlsdorf <dkohlsdorf@gmail.com>
2020-04-16 08:42:56 +0000

    audio

--- ml_pipeline/audio.py
+++ ml_pipeline/audio.py
@@ -90,7 +90,6 @@ def dataset(folder, params, label_func, shuffle):
     for filename in tf.io.gfile.listdir(folder):
         if filename.endswith('.wav') or filename.startswith('cluster') or filename.startswith('noise'):
             path = "{}/{}".format(folder, filename)
-            print(path)
             spec_iter = labeled_spectrogram_windows(path, params, label_func, shuffle=shuffle)
             for x in spec_iter:
                 yield x

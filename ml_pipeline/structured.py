09af7ebfaa0d99e7a0c5d1c3517d46ca339a8723
Daniel Kohlsdorf <daniel.kohlsdorf@xing.com>
2020-05-19 12:55:58 +0200

    Working on hmms

--- ml_pipeline/structured.py
+++ ml_pipeline/structured.py
@@ -206,8 +206,8 @@ def make_hmm(cluster, assignment, overlapping, min_len = 4, min_instances = 1, m
         dists = [Gaussian(mu, std) for i in range(0, 4)]
         logstructure.info("\t Model fit")
         hmm = HiddenMarkovModel(trans_mat, dists)
-        logstructure.info(hmm.transitions)
         for _ in range(0, max_train):
+            logstructure.info(hmm.transitions)
             inference    = [infer.infer(hmm, seq) for seq in x_label]
             zetas        = [bw.infer(hmm, x_label[i], inference[i][1], inference[i][2]) for i in range(0, len(x_label))]    
             gammas       = [gamma for gamma, _, _ in inference]

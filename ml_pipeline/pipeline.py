4ccb3498312dd3e2cc37e61d50b61bbac1ab503b
Daniel Kohlsdorf <dkohlsdorf@gmail.com>
2020-06-11 20:34:22 +0000

    working on params

--- ml_pipeline/pipeline.py
+++ ml_pipeline/pipeline.py
@@ -17,7 +17,7 @@ log.setLevel(logging.INFO)
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
 
-from apache_beam.options.pipeline_options import PipelineOptions, StandardOptions
+from apache_beam.options.pipeline_options import PipelineOptions, StandardOptions, GoogleCloudOptions
 
 from tensorflow.keras.backend import set_learning_phase
 from tensorflow.keras.models import load_model

6e2b43ceb87885b906606337e6f6ecc6b900e2cc
Daniel Kohlsdorf <dkohlsdorf@gmail.com>
2020-09-23 17:32:47 +0000

    worth it

--- ml_pipeline/pipeline.py
+++ ml_pipeline/pipeline.py
@@ -664,7 +664,6 @@ if __name__== "__main__":
         latent          = c['latent']
         batch           = c['batch']
         embedding_batch = c['embedding_batch']
-        epochs          = c['epochs']
         epochs_encoder  = c['epochs_encoder']
         epochs_sup      = c['epochs_sup']
         epochs_finetune = c['epochs_finetune']
@@ -687,21 +686,20 @@ if __name__== "__main__":
         n_writers    = c['n_writers']
         min_len      = c['min_len']
 
-        for i in range(0, epochs):
-            log.info("Mixed Training Epoch AE: {}".format(i))
-            train_auto_encoder(version, unsupervised, output, params, latent, batch, epochs_encoder, conv_param)
-            log.info("Mixed Training Epoch SIL: {}".format(i))
-            train_silence(version, silence, output, params, "{}/encoder.h5".format(output), batch, epochs_sup, conv_param, latent, freeze, transfer=transfer)
-            log.info("Mixed Training Epoch TYPE: {}".format(i))
-            train_type(version, type_class, output, params, "{}/encoder.h5".format(output), batch, epochs_sup, conv_param, latent, freeze, transfer)        
-            log.info("Mixed Training Epoch CLUSTER: {}".format(i))
-            train_clusters(version, 'data/v6_clustering', output, params, "{}/encoder.h5".format(output), batch, epochs_sup, conv_param, latent, freeze, transfer)
-            log.info("Mixed Training Epoch FINE_TUNE: {}".format(i))
-            fine_tuning(unsupervised, output, params, latent, "{}/encoder.h5".format(output), batch, epochs_finetune)             
-            log.info("Mixed Training Epoch CLUSTER_LOSS: {}".format(i))
-            clustering_loss(unsupervised, output, params, latent, "{}/encoder.h5".format(output), batch, epochs_finetune)
-            log.info("Mixed Training Epoch AE: {}".format(i))
-            train_auto_encoder(version, unsupervised, output, params, latent, batch, epochs_encoder, conv_param)
+        log.info("Mixed Training Epoch AE: {}".format(i))
+        train_auto_encoder(version, unsupervised, output, params, latent, batch, epochs_encoder, conv_param)
+        log.info("Mixed Training Epoch SIL: {}".format(i))
+        train_silence(version, silence, output, params, "{}/encoder.h5".format(output), batch, epochs_sup, conv_param, latent, freeze, transfer=transfer)
+        log.info("Mixed Training Epoch TYPE: {}".format(i))
+        train_type(version, type_class, output, params, "{}/encoder.h5".format(output), batch, epochs_sup, conv_param, latent, freeze, transfer)        
+        log.info("Mixed Training Epoch CLUSTER: {}".format(i))
+        train_clusters(version, 'data/v6_clustering', output, params, "{}/encoder.h5".format(output), batch, epochs_sup, conv_param, latent, freeze, transfer)
+        log.info("Mixed Training Epoch FINE_TUNE: {}".format(i))
+        fine_tuning(unsupervised, output, params, latent, "{}/encoder.h5".format(output), batch, epochs_finetune)             
+        log.info("Mixed Training Epoch CLUSTER_LOSS: {}".format(i))
+        clustering_loss(unsupervised, output, params, latent, "{}/encoder.h5".format(output), batch, epochs_finetune)
+        log.info("Mixed Training Epoch AE: {}".format(i))
+        train_auto_encoder(version, unsupervised, output, params, latent, batch, epochs_encoder, conv_param)
 
         evaluate_encoder(version, unsupervised, output, "{}/encoder.h5".format(output), params, viz_k)        
         test_reconstruction(silence, output, params)

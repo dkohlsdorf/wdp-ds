ef646e86231f9d5126a25c5b9413b8eaf02e6ead
Daniel Kohlsdorf <daniel.kohlsdorf@xing.com>
2020-10-02 01:02:12 +0200

    still on it

--- ml_pipeline/sequence_clustering.py
+++ ml_pipeline/sequence_clustering.py
@@ -30,7 +30,7 @@ def distance_compute_job(regions, distance_threshold_frame, warping_band_percent
 
 
 def dtw_process(i, j, ri, rj, w):
-    if i % 100 == 0:
+    if i % 100 == 0 and j % 100 == 0:
         logcluster.info("Process dtw({}, {})".format(i, j, w))
     return dtw(i,j, np.stack(ri), np.stack(rj), w)
     

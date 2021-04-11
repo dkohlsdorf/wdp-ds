60f328589ca2224b412d940df707d3d521f94042
daniel.kohlsdorf <dkohlsdorf@gmail.com>
2021-04-12 01:21:20 +0200

    alignment based

--- lib_dolphin/sequencing.py
+++ lib_dolphin/sequencing.py
@@ -8,8 +8,18 @@ from numba import jit
 from collections import namedtuple
 
 
-Symbol = namedtuple('Symbol', 'id type start stop') 
+class Symbol(namedtuple('Symbol', 'id type start stop')):
+    
+    def __str__(self):
+        return "{}:{}".format(self.id, self.type)
 
+INSERTION = 1
+DELETION  = 2
+MATCH_ID  = 3
+MATCH_T   = 4
+SUBS      = 5
+
+    
 @jit(nopython=True)        
 def max3(a, b, c):    
     x = a
@@ -28,8 +38,9 @@ def similarity(symbol_a, type_a, symbol_b, type_b):
     else:
         return -1.0
 
+    
 @jit(nopython=True)
-def needleman_wunsch(symbols_a, symbols_b, types_a, types_b, gap = -1):        
+def needleman_wunsch(symbols_a, symbols_b, types_a, types_b, gap = -1):      
     N = len(symbols_a)
     M = len(symbols_b)
     dp = np.zeros((N + 1, M + 1))
@@ -43,8 +54,7 @@ def needleman_wunsch(symbols_a, symbols_b, types_a, types_b, gap = -1):
                 dp[i - 1, j] + gap, 
                 dp[i, j - 1] + gap
             )
-    return dp[N, M]
-
+    return dp
         
 class Sequence(namedtuple('Sequence', 'symbols file offset')):
     
@@ -57,12 +67,52 @@ class Sequence(namedtuple('Sequence', 'symbols file offset')):
         return compressed
     
     def similarity(self, other, gap = -1):     
-        a = self.rle
-        b = other.rle
-        return needleman_wunsch(np.array([symbol.id for symbol in a]),
-                                np.array([symbol.id for symbol in b]),
-                                np.array([symbol.type for symbol in a]),
-                                np.array([symbol.type for symbol in b]))
+        a  = self.rle
+        b  = other.rle
+        symbols_a = np.array([symbol.id for symbol in a])
+        symbols_b = np.array([symbol.id for symbol in b])
+        types_a   = np.array([symbol.type for symbol in a])
+        types_b   = np.array([symbol.type for symbol in b])
+        dp = needleman_wunsch(symbols_a, symbol_b, types_a, types_b)
+        i = len(a)
+        j = len(b)
+        path = []
+        while i > 0 and j > 0:
+            op     = DELETE
+            min_dp = dp[i - 1, j] 
+            if dp[i, j - 1] <= min_dp:
+                op     = INSERT
+                max_dp = dp[i, j - 1]
+            if dp[i - 1, j - 1] <= min_dp:
+                op     = MATCH_ID
+                max_dp = dp[i - 1, j - 1]
+            if op == MATCH_ID:
+                if symbols_a[i - 1] == symbols_b[j - 1]:
+                    op = MATCH_ID
+                elif types_a[i - 1] == types_b[j - 1]:
+                    op = MATCH_T
+                else:
+                    op = MATCH
+            path.append([op, x[i - 1], y[j - 1]])
+
+            if op == DELETE:
+                i -= 1
+            elif op == INSERT:
+                j -= 1
+            else:
+                i -= 1
+                j -= 1            
+        while i > 0:
+            path.append([DELETE, x[i - 1], y[j]])
+            i -= 1
+        while j > 0:
+            path.append([INSERT, x[i], y[j - 1]])
+            j -= 1
+        path.reverse()
+        return dp[N, M], path
+
+    def __str__(self):
+        return " ".join([str(symbol) for symbol in self.rle])
         
         
 def extract_id(filename):

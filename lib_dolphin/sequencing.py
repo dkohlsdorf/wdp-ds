035d06e058f44504c288f2a1a627c0a73c73fd92
daniel.kohlsdorf <dkohlsdorf@gmail.com>
2021-04-13 13:37:18 +0200

    working on alignment code

--- lib_dolphin/sequencing.py
+++ lib_dolphin/sequencing.py
@@ -25,7 +25,8 @@ def regions(df, th):
     for i, row in df.iterrows():
         if row['prob'] > th:
             filtered.append(Symbol(row['cluster'], row['knn'], row['start'], row['stop'], row['prob']))
-
+    if len(filtered) == 0:
+        return []
     compressed = []
     current = filtered[0]
     for symbol in filtered[1:]:
@@ -34,7 +35,6 @@ def regions(df, th):
         else:
             compressed.append(current)
             current = symbol
-
     regions = []
     current = []
     for symbol in compressed:
@@ -55,44 +55,85 @@ def max3(a, b, c):
         x = c
     return x
         
-    
+
+@jit(nopython=True)        
+def imax(a, b):    
+    if a > b:
+        return a
+    return b
+
+
+@jit(nopython=True)        
+def imin(a, b):    
+    if a < b:
+        return a
+    return b
+
+
 @jit(nopython=True)
 def similarity(symbol_a, type_a, symbol_b, type_b):
     if symbol_a == symbol_b:
         return 1.0
     elif type_a == type_b:
-        return 0.5
+        return 0.25
     elif type_a[0] == 'E' and type_b[0] == 'B' or type_a[0] == 'B' and type_b[0] == 'E':
-        return 0.25
+        return 0.1
     elif type_a[0] == 'W' and type_b[0] == 'W':
-        return 0.25
+        return 0.1
     else:
         return -1.0
 
     
 @jit(nopython=True)
-def needleman_wunsch(symbols_a, symbols_b, types_a, types_b, gap = -1):    
-    N = len(symbols_a)
+def needleman_wunsch(symbols_a, symbols_b, types_a, types_b, gap, w):   
+    N = len(symbols_a)    
     M = len(symbols_b)
-    dp = np.zeros((N + 1, M + 1))
-    dp[0,0] = 0.0
-    dp[1:, 0] = -np.arange(1, N + 1)
-    dp[0, 1:] = -np.arange(1, M + 1)
-    for i in range(1, N + 1):
-        for j in range(1, M + 1):
+
+    min_val = -imax(N, M)
+    dp = np.ones((N + 1, M + 1)) * min_val
+    dp[0,0] = 0.0    
+    for i in range(1, N + 1):    
+        for j in range(imax(1, i - w), imin(M + 1, i + w)):
             dp[i, j] = max3(
                 dp[i - 1, j - 1] + similarity(symbols_a[i - 1], types_a[i - 1], symbols_b[j - 1], types_b[j - 1]),
                 dp[i - 1, j] + gap, 
                 dp[i, j - 1] + gap
             )
-    
     return dp
 
 
-def score(a, b, gap):
+def score(a, b, gap, band):
     symbols_a = [s.id for s in a]
     symbols_b = [s.id for s in b]
     types_a   = [s.type for s in a]
     types_b   = [s.type for s in b]
-    dp        = needleman_wunsch(symbols_a, symbols_b, types_a, types_b, gap)
+    
+    band_perc = 0.1
+    N = len(symbols_a)    
+    M = len(symbols_b)
+
+    band = imax(N, M) * band_perc    
+    w = imax(band, np.abs(N-M)) + 2
+    w = int(w)
+    
+    print("Band: {}, Band Corrected: {}, N: {}, M: {}".format(band, w, N, M))
+    dp        = needleman_wunsch(symbols_a, symbols_b, types_a, types_b, gap, w)
     return dp[len(symbols_a),len(symbols_b)]
+
+
+def distances(sequences, gap, band):
+    n = len(sequences)
+    similarity = np.zeros((n, n))
+    
+    for i in range(0, n):
+        print("Processing: {}".format(i))
+        for j in range(i + 1, n):
+            a = sequences[i]
+            b = sequences[j]
+            s = score(a, b, gap, band)
+            similarity[i][j] = s
+            similarity[j][i] = s
+    minsim   = np.min(similarity) 
+    maxsim   = np.max(similarity) 
+    distance = 1.0 - (similarity - minsim) / (maxsim - minsim)
+    return distance

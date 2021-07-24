76a4e982f2340ec37b1c1a3119fc6b75cda15589
Daniel Kohlsdorf <dkohlsdorf@gmail.com>
2021-07-25 00:36:25 +0200

    fixing errors

--- pipeline.py
+++ pipeline.py
@@ -404,6 +404,7 @@ def htk_continuous(folder, htk, noise, hmm, epochs=10, components=10):
 
 def sequencing(audio, folder, htk ,outfolder):
     print("SEQUENCING")
+    
     out       = check_output(["rm", "-rf", outfolder])
     out       = check_output(["mkdir", outfolder])
     out       = check_output(["mkdir", "{}/images".format(outfolder)]) 
@@ -422,6 +423,7 @@ def sequencing(audio, folder, htk ,outfolder):
         .split(" ")
     cmd.extend(htk_files)
     out = check_output(cmd)    
+    
     annotations = parse_mlf('{}/sequenced.lab'.format(outfolder))
     th = htk_threshold('{}/sequenced.lab'.format(outfolder), outfolder)    
     plot_annotations(annotations, audio, "{}/images".format(outfolder), T // 2, th)

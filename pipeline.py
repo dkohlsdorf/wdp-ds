05e6f135f5c9ee96649781b009dfea37553ccba8
Daniel Kohlsdorf <dkohlsdorf@gmail.com>
2021-10-10 14:53:58 +0200

    set retraining true

--- pipeline.py
+++ pipeline.py
@@ -152,7 +152,7 @@ def neighbours_encoder(encoder, x_train, y_train, x_test, y_test, label_dict, na
     return accuracy
 
     
-def train(label_file, wav_file, out_folder="output", perc_test=0.33, retrain = False, super_epochs=3, relabel=False, resample=10000):
+def train(label_file, wav_file, out_folder="output", perc_test=0.33, retrain=True, super_epochs=3, relabel=False, resample=10000):
     instances, ra, labels, label_dict = dataset_supervised_windows(
         label_file, wav_file, lo=FFT_LO, hi=FFT_HI, win=FFT_WIN, step=FFT_STEP, raw_size=RAW_AUDIO)    
     reverse = dict([(v, k) for k, v in label_dict.items()])

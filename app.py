958d31d4dd6dd4cc9fb8186de686dd25f4f6d525
Daniel Kohlsdorf <dkohlsdorf@gmail.com>
2022-03-02 21:39:14 +0100

    building a login system

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import re
 import datetime
 import pickle
+import flask
+import flask_login
 
 from decoder_worker import DiscoveryService
 from flask import Flask, render_template
@@ -9,19 +11,69 @@ from flask import Flask, render_template
 SEQ_PATH = '../web_service/sequences/'
 IMG_PATH = '../web_service/images/'
 PKL_PATH = '../web_service/service.pkl'
+USERS    = {'dolphin-visitor': {'password' : 'stenella_frontalis'}}
+SECRET   = 'wdp-ds-dolphin' 
 
 
 try:
     DISCOVERY = pickle.load(open(PKL_PATH, "rb"))
 except (OSError, IOError) as e:
     DISCOVERY = DiscoveryService(SEQ_PATH)
-    pickle.dump(foo, open(PKL_PATH, "wb"))
+    pickle.dump(DISCOVERY, open(PKL_PATH, "wb"))
     
 
 app = Flask(__name__,
             static_url_path = '', 
             static_folder   = IMG_PATH,
             template_folder = 'templates')
+app.secret_key = SECRET
+
+
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
+
+
+class User(flask_login.UserMixin):
+    pass
+
+
+@login_manager.user_loader
+def user_loader(email):
+    if email not in USERS:
+        return
+    user = User()
+    user.id = email
+    return user
+
+
+@login_manager.request_loader
+def request_loader(request):
+    email = request.form.get('email')
+    if email not in USERS:
+        return
+    user = User()
+    user.id = email
+    return user
+
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if flask.request.method == 'GET':
+        return '''
+               <h1> [:WDP:] Dolphin Discovery Tool </h1>
+               <form action='/' method='POST'>
+                <input type='text' name='email' id='email' placeholder='email'/>
+                <input type='password' name='password' id='password' placeholder='password'/>
+                <input type='submit' name='submit'/>
+               </form>
+               '''    
+    email = flask.request.form['email']
+    if flask.request.form['password'] == USERS[email]['password']:
+        user = User()
+        user.id = email
+        flask_login.login_user(user)
+        return flask.redirect(flask.url_for('discovery'))
+    return 'Bad login'
 
 
 def process_sequence(s):
@@ -41,8 +93,10 @@ def process_sequence(s):
         "sequence": sequence,
         "time"    : time
     }
-    
+
+
 @app.route('/discovery')
+@flask_login.login_required
 def discovery():
     s         = DISCOVERY.sample()
     sequences = [process_sequence(s[0])] + [process_sequence(x) for x in s[1][1:]]
@@ -50,6 +104,7 @@ def discovery():
 
 
 @app.route('/neighborhood/<key>')
+@flask_login.login_required
 def neighborhood(key):
     key       = int(key)
     s         = DISCOVERY.get(key)

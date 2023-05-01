8fd45bedaed916139a45b5963f4bbfd5858cb615
Daniel Kohlsdorf <dkohlsdorf@gmail.com>
2023-05-01 16:29:23 +0200

    dtw indexing treee

--- lib_dolphin/indexing.py
+++ lib_dolphin/indexing.py
@@ -1,13 +1,29 @@
+"""
+Building A DTW Indexing Tree
+
+Idea: 
+  We select a random time series. Then we compute all distances to that series.
+  We sample a second series proportional to the distances squared. We partition the
+  data around the two sequences and recursively partition the resulting sub partitions.  
+
+USAGE:
+  X = np.random.uniform(size=(100, 10, 10)) * 100
+  root = build_tree(X, dtw_selector)
+  explore(root)        
+  print(search(X[0], root))
+"""
+
 import numpy as np
+import random
 
 from collections import namedtuple
 from numba import jit
 
-#from lib_dolphin.dtw import *
+from lib_dolphin.dtw import *
 IndexRange = namedtuple("IndexingRange", "start stop")
 
 
-class KWayIndexingTree:
+class IndexingTree:
 
     def __init__(self):
         self.selector = None
@@ -16,10 +32,22 @@ class KWayIndexingTree:
 
     def leaf(self):
         return self.left is None and self.right is None
+
+@jit
+def search(x, tree):
+    queue = [tree]
+    while len(queue) > 0:
+        node = queue.pop()        
+        if node.leaf():
+            return node.selector
+        if node.selector(x) == 0:
+            queue.append(node.left)
+        else:
+            queue.append(node.right)
+            
     
 @jit        
 def partition(ids, idx, lr):
-
     i = idx.start - 1
     for j in range(idx.start, idx.stop):
         if lr[j] == 0:
@@ -34,7 +62,7 @@ def build_tree(data, make_selector, n_leaf=10):
     N = len(data)
     ids = np.arange(0, N)
     selections = np.zeros(N)
-    root = KWayIndexingTree()
+    root = IndexingTree()
     ranges = [(IndexRange(0, N), root)]
     
     while len(ranges) > 0:
@@ -42,9 +70,9 @@ def build_tree(data, make_selector, n_leaf=10):
         if i.stop - i.start >= n_leaf:
             node.selector = make_selector(data, ids, i)
             for j in range(i.start, i.stop):
-                selections[j] = node.selector(data, j)
-            node.left = KWayIndexingTree()
-            node.right = KWayIndexingTree()
+                selections[j] = node.selector(data[ids[j]])
+            node.left = IndexingTree()
+            node.right = IndexingTree()
             l, r = partition(ids, i, selections)
             ranges.append((l, node.left))
             ranges.append((r, node.right))
@@ -52,28 +80,17 @@ def build_tree(data, make_selector, n_leaf=10):
             node.selector = ids[i.start:i.stop]
     return root
 
+@jit
+def dtw_selector(X, ids, idx):
+    n = idx.stop - idx.start
+    selected_idx = np.random.randint(n) + idx.start
+    distances = np.array([dtw(X[selected_idx], X[i]) for i in range(idx.start, idx.stop) if i != selected_idx])
+    distances = distances ** 2
+    distances = distances / sum(distances)
+    selected_idx_sample = random.choices([ids[i] for i in range(idx.start, idx.stop) if i != selected_idx], distances)[0]
+    selector = lambda x: 0 if dtw(X[selected_idx], x) <= dtw(X[selected_idx_sample], x) else 1
+    return selector
 
-        
-
-
-# Test me ======================================================================
-
-def test_selector(data, ids, idx):
-    selector = lambda x, j: j % 2
-    return selector 
-
-x = [1,2,3,4,5,6]
-y = [0,1,0,1,0,1]
-
-idx1 = IndexRange(0, 6)
-
-l, r = partition(x, idx1, y)
-print(l, r, x, y)
-
-print(x[l.start:l.stop], y[l.start:l.stop])
-print(x[r.start:r.stop], y[r.start:r.stop])
-    
-root = build_tree(np.zeros((100, 10)), test_selector)
 
 def explore(node, indent=''):
     if node.leaf():
@@ -82,5 +99,3 @@ def explore(node, indent=''):
         print(f"{indent}node")
         explore(node.left, f"{indent} ")
         explore(node.right, f"{indent} ")
-
-explore(root)        
